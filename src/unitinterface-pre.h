151d01c8e6a5282b7991eece1179d61f3eb9c609
alex <alex@d57568f3-fe0c-0410-b608-e86116ac1d00>
2008-09-24 19:02:55 +0000

    Relicense complete project to GPLv3
    git-svn-id: https://bloopz.zapto.org/svn/nightfall/trunk@1279 d57568f3-fe0c-0410-b608-e86116ac1d00

--- src/unitinterface-pre.h
+++ src/unitinterface-pre.h
@@ -1,3 +1,23 @@
+/*
+ * Nightfall - Real-time strategy game
+ *
+ * Copyright (c) 2008 Marcus Klang, Alexander Toresson and Leonard Wickmark
+ * 
+ * This file is part of Nightfall.
+ * 
+ * Nightfall is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Nightfall is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Nightfall.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef __UNITINTERFACE_H_PRE__
 #define __UNITINTERFACE_H_PRE__
 
